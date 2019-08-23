78156f7ebf69ee51aa96d99278532b7d4d463ffe
shiyingying01 <718275303@qq.com>
2019-08-23 10:07:36 +0800

    practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,28 @@
 package practice02;
 
 public class Student {
+	private String name;
+	private int age;
+	private int klass;
+	public Student(String name,int age,int klass){
+		this.name = name;
+		this.age = age;
+		this.klass = klass;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public int getKlass() {
+		return this.klass;
+	}
+	
+	public String introduce() {
+		return "I am a Student"  + ". I am at Class " + this.klass + ".";
+	}
 }
