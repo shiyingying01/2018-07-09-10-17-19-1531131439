4365ce2d97e846bd110bfd16846988475a8a0e67
shiyingying01 <718275303@qq.com>
2019-08-23 10:22:32 +0800

    practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,22 @@
 package practice04;
 
 public class Worker {
+	private String name;
+	private int age;
+	public Worker(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String introduce() {
+		return "My name is " + this.name + ". I am " + this.age + " years old." + " I am a Worker. " + "I have a job." ;
+	}
 }
