ea5f2e57cdde823deb00341a8f66243cded9ff43
shiyingying01 <718275303@qq.com>
2019-08-27 15:06:58 +0800

    test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -5,13 +5,13 @@ public class Teacher {
 	private String name;
 	private int age;
 	private int klass;
-	
-	public Teacher(String name,int age,int klass){
+
+	public Teacher(String name, int age, int klass) {
 		this.name = name;
 		this.age = age;
 		this.klass = klass;
 	}
-	
+
 	public Teacher(String name, int age) {
 		super();
 		this.name = name;
@@ -21,21 +21,22 @@ public class Teacher {
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public int getAge() {
 		return this.age;
 	}
-	
+
 	public int getKlass() {
 		return this.klass;
 	}
-	
+
 	public String introduce() {
-		if(klass == '0') {
-			return "My name is " + this.name + ". I am " + this.age + " years old." +" I am a Taecher"  + ". I teach No Class." ;	
+		if (klass == '0') {
+			return "My name is " + this.name + ". I am " + this.age + " years old." + " I am a Taecher"
+					+ ". I teach No Class.";
+		} else {
+			return "My name is " + this.name + ". I am " + this.age + " years old." + " I am a Taecher"
+					+ ". I teach Class " + this.klass + ".";
+		}
 	}
-		{
-		return "My name is " + this.name + ". I am " + this.age + " years old." +" I am a Taecher"  + ". I teach Class " + this.klass + ".";
-			}
-}
 }
